1e61f952e6fbb3baef8413170096f48be5660035
Compu5 <Compu5@Compu5>
2026-03-25 15:19:18 -0600

    Cuento dos

---
+++ src/formenu/Cuento.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package formenu;
+
+/**
+ *
+ * @author Compu5
+ */
+public class Cuento {
+        /*
+    String Cuento(String nombre, String edad,String grado, String estado, String comidaf, String colorf) {
+        
+        String  cuento = "Erase una vez un joven llamado "+nombre+" que vivía en "+estado+" que a sus "+edad+" años, decidio aventurarse a algo mas alla de lo ordinario. Asi que, un día de esos que suelen pasar desapercibidas, se entero de un lugar misterioso en donde abundaba la/el "+ comidaf+", En donde los atardeceres, elcielo era de un color "+colorf+" .Todo lo ideal para alguien que le gustase todo aquello. En pocas palabras, un paraiso terrenal, pero las tareas de "+grado+" grado, le impedian aventurarse a buscar aquel misterioso lugar que tanto curiosidad le generaba, esperando que algun dia ";
+        return cuento;
+    }
+    */    
+
+
+}
